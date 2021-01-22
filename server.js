ddf857878cceee19d50c6ed6cd964e727b9aa57c
Dennis <dmcwhinney@myseneca.ca>
2021-01-21 19:00:45 -0500

    edited file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Dennis McWhinney - 105843197");
 });
 
 // setup http server to listen on HTTP_PORT
